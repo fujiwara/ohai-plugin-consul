3c5215dab403ed769c545293e5dab91f2c5127a0
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-06-03 13:24:25 +0900

    split test case

--- test/test_consul.rb
+++ test/test_consul.rb
@@ -3,40 +3,43 @@ require "ohai"
 require "tmpdir"
 require "glint"
 
+Ohai::Config[:plugin_path] << "./lib"
+server = Glint::Server.new(8500, { :timeout => 3 }) do |port|
+  dir = Dir.tmpdir
+  exec "consul", "agent", "-data-dir", dir, "-server", "-bootstrap"
+end
+server.start
+$o = Ohai::System.new
+$o.all_plugins
+puts $o[:consul]
+
 class TestOhaiPluginConsul < Test::Unit::TestCase
   def setup
-    server = Glint::Server.new(8500, { :timeout => 3 }) do |port|
-      dir = Dir.tmpdir
-      exec "consul", "agent", "-data-dir", dir, "-server", "-bootstrap"
-    end
-    server.start
-
-    Ohai::Config[:plugin_path] << "./lib"
-    @o = Ohai::System.new
+    @consul = $o[:consul]
   end
 
-  def test_consul
-    @o.all_plugins
-    consul = @o[:consul]
-    assert_not_nil consul
-
-    # agent
-    assert_equal({}, consul[:agent][:checks])
-    assert_equal 1, consul[:agent][:members].size
-
-    # catalog
-    assert_equal(["dc1"], consul[:catalog][:datacenters])
-    assert_equal(1, consul[:catalog][:nodes].size)
-    node_name = consul[:catalog][:nodes][0]["Node"]
-    assert_not_nil consul[:catalog][:node][node_name]
-
-    assert_equal({"consul" => nil}, consul[:catalog][:services])
-    assert_not_nil consul[:catalog][:service]["consul"]
-
-    # status
-    assert_not_nil consul[:status][:leader]
-    assert_not_nil consul[:status][:peers]
-
-    puts consul
+  def test_base
+    assert_not_nil @consul
   end
+
+  def test_agent
+    assert_equal({}, @consul[:agent][:checks])
+    assert_equal 1, @consul[:agent][:members].size
+  end
+
+  def test_catalog
+    assert_equal(["dc1"], @consul[:catalog][:datacenters])
+    assert_equal(1, @consul[:catalog][:nodes].size)
+    node_name = @consul[:catalog][:nodes][0]["Node"]
+    assert_not_nil @consul[:catalog][:node][node_name]
+
+    assert_equal({"consul" => nil}, @consul[:catalog][:services])
+    assert_not_nil @consul[:catalog][:service]["consul"]
+  end
+
+  def test_status
+    assert_not_nil @consul[:status][:leader]
+    assert_not_nil @consul[:status][:peers]
+  end
+
 end
